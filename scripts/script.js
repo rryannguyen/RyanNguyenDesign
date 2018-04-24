692e31aeb4966245e2763bf275cd61879786de85
rryannguyen <anguyen@risd.edu>
2018-04-23 22:34:52 -0400

    omg

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
+$(document).ready(function() {
+  var bodyWidth = document.body.clientWidth
+  var bodyHeight = document.body.clientHeight;
+
+  $('.random').each(function(idx, img) {
+    var randPosX = Math.floor((Math.random() * bodyWidth));
+    var randPosY = Math.floor((Math.random() * bodyHeight));
+    console.log(randPosY);
+    $(img).css('left', randPosX);
+    $(img).css('top', randPosY);
+
+  });
+});
 
 
 
